6fc74290e771790244b7ad01f4913d2c4217ea05
ch3 <ch3mailbox.org>
2022-10-14 12:16:06 +0200

    Add more precise tests for the chunker

--- tests/test_chunker.py
+++ tests/test_chunker.py
@@ -7,42 +7,36 @@ class TestChunker(unittest.TestCase):
         self.assertIsNotNone(Chunker(2))
 
     def test_boundary(self):
-        # This is not quite true - the test results are adapted to the chunker
+        # This is not quite true - the test results are adapted to the chunker!
         # that's a bit off, but in general do work as a sanity check
-        # FIXME: The code below needs to be reviewd with a clear head
         c = Chunker(2)
-        self.assertTrue(c.isBoundary(b'\x00'*64))
-        self.assertTrue(c.isBoundary(b'\x00'*63 + b'\x01'*1))
-        self.assertTrue(c.isBoundary(b'\x00'*16 + b'\xff'*48))
-        self.assertTrue(c.isBoundary(b'\x00'*32 + b'\xff'*32))
-        self.assertTrue(c.isBoundary(b'\x00'*48 + b'\xff'*16))
+        self.assertTrue (c.isBoundary(b'\x00'*64))
+        self.assertTrue (c.isBoundary(b'\x00'*63 + b'\x01'*1))
+        self.assertTrue (c.isBoundary(b'\x00'*16 + b'\xff'*48))
+        self.assertTrue (c.isBoundary(b'\x00'*32 + b'\xff'*32))
+        self.assertTrue (c.isBoundary(b'\x00'*48 + b'\xff'*16))
+        self.assertTrue (c.isBoundary(b'\x00'*2  + b'\xff'*62))
+        self.assertTrue (c.isBoundary(b'\x00\xfe' + b'\xff'*62))
+        self.assertFalse(c.isBoundary(b'\x00'*1  + b'\xff'*63))
         self.assertFalse(c.isBoundary(b'\xff'*64))
 
         c = Chunker(32)
-        self.assertTrue(c.isBoundary(b'\x00'*64))
-        self.assertTrue(c.isBoundary(b'\x00'*63 + b'\x01'*1))
-        self.assertTrue(c.isBoundary(b'\x00'*48 + b'\xff'*16))
-        self.assertTrue(c.isBoundary(b'\x00'*32 + b'\xff'*32))
+        self.assertTrue (c.isBoundary(b'\x00'*64))
+        self.assertTrue (c.isBoundary(b'\x00'*63 + b'\x01'*1))
+        self.assertTrue (c.isBoundary(b'\x00'*48 + b'\xff'*16))
+        self.assertTrue (c.isBoundary(b'\x00'*32 + b'\xff'*32))
+        self.assertTrue (c.isBoundary(b'\x00'*31 + b'\xfe' + b'\xff'*32))
+        self.assertFalse(c.isBoundary(b'\x00'*31 + b'\xff' + b'\xff'*32))
         self.assertFalse(c.isBoundary(b'\x00'*16 + b'\xff'*48))
         self.assertFalse(c.isBoundary(b'\xff'*64))
 
         c = Chunker(64)
-        self.assertTrue(c.isBoundary(b'\x00'*64))
-        self.assertTrue(c.isBoundary(b'\x00'*63 + b'\x01'*1))
+        self.assertTrue (c.isBoundary(b'\x00'*64))
+        self.assertTrue (c.isBoundary(b'\x00'*63 + b'\xfe'*1))
+        self.assertFalse(c.isBoundary(b'\x00'*63 + b'\xff'*1))
         self.assertFalse(c.isBoundary(b'\x00'*48 + b'\xff'*16))
         self.assertFalse(c.isBoundary(b'\x00'*32 + b'\xff'*32))
         self.assertFalse(c.isBoundary(b'\x00'*16 + b'\xff'*48))
         self.assertFalse(c.isBoundary(b'\xff'*64))
 
-
-    #def test_put_get_single(self):
-    #    n = Node()
-    #    self.assertIsNone(n.put(bytes(0), b'1337'))
-    #    self.assertEqual(n.get(bytes(0)), b'1337')
-
-    #def test_put_get_multi(self):
-    #    n = Node()
-    #    self.assertIsNone(n.put(b'0', b'1337'))
-    #    self.assertIsNone(n.put(b'1', b'31337'))
-    #    self.assertEqual(n.get(b'0'), b'1337')
-    #    self.assertEqual(n.get(b'1'), b'31337')
+# TODO generate random byte arrays and check whether the statistics hold
